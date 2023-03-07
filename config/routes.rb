c06ee698d349c0ee19fe5867b3c89474b5243f4f
Aaron <amstadler@protonmail.com>
2023-03-06 19:27:11 -0500

    fix petsshow route not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pets#index'
   get 'pets/index', to: 'pets#index'
-  get 'pets/:id', to: 'pets#show', as: :pet_show, constraints: {id: /[d+]/}
+  get 'pets/:id', to: 'pets#show', as: :pet_show, constraints: {id: /[\d+]/}
   get 'pets/search', to: 'pets#search', as: :pets_search
   get 'pets/updatesearch', to: 'pets#update_search', as: :update_search 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
